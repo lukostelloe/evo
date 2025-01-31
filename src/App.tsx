3a66d86a88c0602de55f1d6aec0e3930488f8a0e
Luke Costelloe <lukostelloe@gmail.com>
2025-01-31 11:21:26 +0100

    🌏 add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@
 import { Button } from "@/components/ui/button"
 import { XStack, YStack } from './components/ui/stacks'
 import SiteHeader from './components/site-header'
+import { useTranslation } from "react-i18next"
+import LanguageSwitcher from "./components/language-switcher"
 
 
 function App() {
   // rgb(238, 228, 202)
   // rgb(253, 246, 232)
 
+  const {t} = useTranslation()
+
 
   return (
     <YStack className='bg-[#FDF6E8] items-center w-screen h-screen text-slate-800 font-sans justify-center'>
       <SiteHeader/>
+      <LanguageSwitcher/>
       <YStack className='gap-2'>
         <p className='font-bold'>Hey, I'm</p>
+        <h1>{t('welcome')}</h1>
         <h1 className="text-5xl font-bold text-black">Luke Costelloe</h1>
         <h1 className="text-3xl font-bold text-black">Creative Developer</h1>
         <p>I'm a developer based in Nice, specialising in Front-End development. I love making things.</p>
---
+++ src/i18n.ts
@@ -0,0 +1,26 @@
+// src/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Initialize i18next
+i18n
+  .use(Backend) // Use backend plugin for loading translations
+  .use(LanguageDetector) // Auto detect language
+  .use(initReactI18next) // Initialize react-i18next
+  .init({
+    fallbackLng: 'en', // Default language
+    debug: true, // Log information to the console (for development)
+    interpolation: {
+      escapeValue: false, // React already escapes values
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}.json', // Path to translation files in the `public` folder
+    },
+    react: {
+      useSuspense: false, // Optional: set it to `true` if you use Suspense
+    },
+  });
+
+export default i18n;
