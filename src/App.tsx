5dbb87c85e2350d30736b614c760f576cb114fbd
Luke Costelloe <lukostelloe@gmail.com>
2025-02-03 14:50:26 +0100

    🎨

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { YStack } from './components/ui/stacks';
 import ButtonTabs from './components/ui/buttonTabs';
-import Intro from './components/intro';
 
 
 export default function App() {
 
   return (
     <YStack className={`bg-[var(--background)] items-center w-screen h-screen text-slate-800 font-sans gap-3 pt-[150px] w-80% px-20`}>
-      <Intro />
       <ButtonTabs />
     </YStack>
   );
--- src/components/aboutMeCard.tsx
+++ src/components/aboutMeCard.tsx
@@ -1,6 +1,7 @@
 import Typewriter from './typewriter';
 import { useTranslation } from 'react-i18next';
 import { XStack, YStack } from './ui/stacks';
+import Intro from './intro';
 
 const useContactConfig = () => {
   const { t } = useTranslation();
@@ -40,17 +41,12 @@ function AboutMeCard() {
 
   return (
     <YStack className='items-start gap-2'>
-      <img width={40} height={40} src='/luke-id.jpeg' alt='luke' />
+      <Intro />
+      <img width={60} height={60} className='rounded-full' src='/luke-id.jpeg' alt='luke' />
       <XStack className='w-[800px]'>
         <Typewriter text={t('about_me_text')} delay={20} />
       </XStack>
       <XStack className='bg-white p-5 border border-black rounded-full items-center gap-4'>
-        <img
-          width={70}
-          src='/luke-id.jpeg'
-          alt='luke-id'
-          className='rounded-full'
-        />
         <XStack className='gap-5'>
           {contactConfig.map((contact) => (
             <Contact img={contact.img} link={contact.link} />
