5bf1042d0052d4e382e3c61130c7fd1664586787
Luke Costelloe <lukostelloe@gmail.com>
2025-02-03 11:13:42 +0100

    🎨 confetti

--- src/components/ui/buttonTabs.tsx
+++ src/components/ui/buttonTabs.tsx
@@ -1,13 +1,15 @@
 import { AtSign, CircleCheck, Code, Download, Smile } from 'lucide-react';
 import { Button } from './button';
-import { XStack } from './stacks';
+import { XStack, YStack } from './stacks';
 import { useState } from 'react';
 import AboutMeCard from '../aboutMeCard';
 import ExperienceCard from '../experienceCard';
 import ContactCard from '../contactCard';
 import TechStackCard from '../techStackCard';
+import ConfettiExplosion from 'react-confetti-explosion';
 
 export default function ButtonTabs() {
+  const [isExploding, setIsExploding] = useState(false);
   const [activeCard, setActiveCard] = useState<string>('aboutMe');
   const handleButtonClick = (card: string) => {
     setActiveCard(card);
@@ -15,6 +17,12 @@ export default function ButtonTabs() {
 
   const activeStyles =
     'bg-yellow-300 hover:bg-yellow-300 translate-y-[2px] shadow-[0px_0px_0px_0px_rgba(0,0,0,1)] border-2';
+
+
+    function handleExplode(){
+      setIsExploding(true)
+      console.log(isExploding)
+    }
 
   return (
     <>
@@ -51,10 +59,16 @@ export default function ButtonTabs() {
           <AtSign />
           Contact
         </Button>
-        <Button variant='orangeround'>
+        <YStack className='justify-center items-center'>
+        <Button variant='orangeround' onClick={handleExplode} className='z-10'>
           <Download />
           Download my CV
         </Button>
+        {isExploding && <ConfettiExplosion zIndex={9} width={500} duration={2000} onComplete={()=>setIsExploding(false)}/>}
+        </YStack>
+
+
+
       </XStack>
       <XStack className='p-10'>
         {activeCard === 'aboutMe' && <AboutMeCard />}
